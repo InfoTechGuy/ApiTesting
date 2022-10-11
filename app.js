e416be994b01858f6869c27f4cb6505037a14f29
Sagad <normel.sajad2013x@gmail.com>
2022-10-11 22:30:33 +0300

    add nice in a comment

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ let courses=[
     {id:0,title:'API'},
     {id:1,title:'Javascript'}
 ];
-
+//nice
 let bodyParser=require('body-parser');
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json())
